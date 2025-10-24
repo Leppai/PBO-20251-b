3a972de509ae696c8c94ee2ef0295e8589376c54
Leppai <levifazli29@gmail.com>
2025-10-24 18:32:46 +0700

    cek pertemuan5

--- lingkaran.php
+++ lingkaran.php
@@ -5,31 +5,13 @@ class LuasLingkaran{
 
     public int $r;
 
-    public function __construct($rValue = 1) {
-        echo "ini dari construct WHEN YAHHH  <br>";
-        $this->r = $rValue;
-    }
-
-    public function tampil($nama = 'ban') {
-        $rumus = LuasLingkaran::phi * ($this->r * $this->r);
-        echo "Lingkaran {$nama} ini hasil nya adalah: {$rumus}";
-    }
-
-    public static function testing() {
-        echo "<br/>";
-        echo "Ini testing static";
-    }
-
-    public function __destruct() {
-        echo "<br> udah ah capee";
-    }
 }
 
 $lingkaran = new LuasLingkaran(20);
-//$lingkaran->r = 10;
+$lingkaran->r = 10;
 $lingkaran->tampil('roda');
 
 LuasLingkaran::testing();
 
-// $rumus = LuasLingkaran::phi * ($lingkaran->r * $lingkaran->r);
-// echo "Hasil nya adalah: " . $rumus;
+$rumus = LuasLingkaran::phi * ($lingkaran->r * $lingkaran->r);
+echo "Hasil nya adalah: " . $rumus;
