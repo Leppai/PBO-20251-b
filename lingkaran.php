836deebd6aae8fa96294801da01933db1e535d20
Leppai <levifazli29@gmail.com>
2025-10-24 18:50:08 +0700

    constrct destruct pertemuan5

--- lingkaran.php
+++ lingkaran.php
@@ -5,13 +5,28 @@ class LuasLingkaran{
 
     public int $r;
 
+    public function __construct($rValue = 2) {
+        echo "ini dari construct WHEN YAHHH  <br>";
+        $this->r = $rValue;
+    }
+
+    public function tampil($nama = 'ban') {
+        $rumus = LuasLingkaran::phi * ($this->r * $this->r);
+        echo "Lingkaran {$nama} ini hasil nya adalah: {$rumus}";
+    }
+
+    public static function testing() {
+        echo "<br/>";
+        echo "Ini testing static 🎉";
+    }
+
+    public function __destruct() {
+        echo "<br> udah ah capee malasss";
+    }
 }
 
-$lingkaran = new LuasLingkaran(20);
-$lingkaran->r = 10;
-$lingkaran->tampil('roda');
+$lingkaran = new LuasLingkaran(14);
+// $lingkaran->r = 12;
+$lingkaran->tampil('roda'); //method tampil
 
-LuasLingkaran::testing();
-
-$rumus = LuasLingkaran::phi * ($lingkaran->r * $lingkaran->r);
-echo "Hasil nya adalah: " . $rumus;
+LuasLingkaran::testing(); //static method
