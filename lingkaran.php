13119c459c1e1f13d6ef1102ac5122f3ed599f57
Leppai <levifazli29@gmail.com>
2025-10-31 08:34:07 +0700

    protected private

--- lingkaran.php
+++ lingkaran.php
@@ -3,10 +3,14 @@
 class LuasLingkaran{
     public const phi = 3.14;
 
-    public int $r;
+    protected int $r;
 
-    public function __construct($rValue = 2) {
-        echo "ini dari construct WHEN YAHHH  <br>";
+    // public function __construct($rValue = 2) {
+    //     echo "ini dari construct WHEN YAHHH  <br>";
+    //     $this->r = $rValue;
+    // }
+
+    function tampil2($rValue = 1) {
         $this->r = $rValue;
     }
 
@@ -20,13 +24,14 @@ class LuasLingkaran{
         echo "Ini testing static 🎉";
     }
 
-    public function __destruct() {
-        echo "<br> udah ah capee malasss";
-    }
+    // public function __destruct() {
+    //     echo "<br> udah ah capee malasss";
+    // }
 }
 
-$lingkaran = new LuasLingkaran(14);
+$lingkaran = new LuasLingkaran();
 // $lingkaran->r = 12;
+$lingkaran->tampil2(10);
 $lingkaran->tampil('roda'); //method tampil
 
-LuasLingkaran::testing(); //static method
+// LuasLingkaran::testing(); //static method
