8f71ca5b23084083d5e291fbde3445ab14f9ccbc
Leppai <levifazli29@gmail.com>
2025-11-07 20:36:47 +0700

    first commit

---
+++ LuasLingkaran.php
@@ -0,0 +1,17 @@
+<?php
+
+class LuasLingkaran {
+
+    public const phi = 3.14;
+
+    public int $jari;
+
+}
+
+$lingkaran = new LuasLingkaran();
+$lingkaran->jari = 10;
+
+$rumus = LuasLingkaran::phi * ($lingkaran->jari * $lingkaran->jari);
+
+echo "Hasil nya adalah: " . $rumus;
+?>
